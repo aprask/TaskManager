8ec1f12e9784bc7eed2b712fe7e0427b9b453044
aprask <a.praskala@tutanota.com>
2024-02-01 15:34:28 -0500

    Added retrieveAllTasks, save, and findTaskByID

--- springtaskmanager/src/main/java/com/taskmanager/springtaskmanager/Repository/TaskRepositoryImpl.java
+++ springtaskmanager/src/main/java/com/taskmanager/springtaskmanager/Repository/TaskRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.taskmanager.springtaskmanager.Repository;
 
+import com.taskmanager.springtaskmanager.Configuration.DatabaseConfig;
 import com.taskmanager.springtaskmanager.Model.Task;
 import org.hibernate.annotations.processing.SQL;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class TaskRepositoryImpl implements TaskRepository {
@@ -16,9 +18,9 @@ public class TaskRepositoryImpl implements TaskRepository {
 
     @Override
     public Task findTaskByID(Long id) {
-        String SQLCommand = "SELECT * FROM users WHERE id = ?";
+        String sqlCommand = "SELECT * FROM tasks WHERE id = ?";
         try{
-            return jdbcTemplate.queryForObject(SQLCommand, new Object[]{id}, new BeanPropertyRowMapper<>(Task.class));
+            return jdbcTemplate.queryForObject(sqlCommand, new Object[]{id}, new BeanPropertyRowMapper<>(Task.class));
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
         }
@@ -26,12 +28,30 @@ public class TaskRepositoryImpl implements TaskRepository {
 
     @Override
     public ArrayList<Task> retrieveAllTasks() {
-        return null;
+        String sqlCommand = "SELECT * FROM tasks";
+        try{
+            return (ArrayList<Task>) jdbcTemplate.query(sqlCommand, new BeanPropertyRowMapper<>(Task.class));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void save(Task task) {
-
+        String sqlCommand = "INSERT INTO tasks (id, title, description, due_date, status) " +
+                "+ VALUES (?, ?, ?, ?, ?)";
+        try{
+            jdbcTemplate.update(
+                    sqlCommand,
+                    task.getTitle(),
+                    task.getDescription(),
+                    task.getDueDate(),
+                    task.isComplete()
+            );
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        jdbcTemplate.execute(sqlCommand);
     }
 
     @Override
