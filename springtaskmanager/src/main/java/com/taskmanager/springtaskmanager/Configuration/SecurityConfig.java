6e58d497cc78616bbdc57ece9b1ec1aff564845a
aprask <a.praskala@tutanota.com>
2024-02-13 11:39:41 -0500

    removed SecurityConfig.java

--- springtaskmanager/src/main/java/com/taskmanager/springtaskmanager/Configuration/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.taskmanager.springtaskmanager.Configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.*;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-}
